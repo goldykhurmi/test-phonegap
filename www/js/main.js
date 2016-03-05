bbccee2027ec44f5f65d70fe9e3a99937b5332b5
Sukhbir Singh <goldykhurmi@gmail.com>
2016-03-05 17:53:16 +0530

    -

--- www/js/main.js
+++ www/js/main.js
@@ -11,14 +11,9 @@ var app = {
                 $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
             }
         });
-    },*/
+},*/
 
-    findByName: function() {
-    var self = this;
-    this.store.findByName($('.search-key').val(), function(employees) {
-        $('.employee-list').html(self.employeeLiTpl(employees));
-    });
-    },
+
 
 
 
@@ -31,21 +26,42 @@ var app = {
             "</div>"
     $('body').html(html);
     $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },*/
-
-    renderHomeView: function() {
-    $('body').html(this.homeTpl());
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
+},*/
 
 
-    showAlert: function (message, title) {
-      if (navigator.notification) {
-        navigator.notification.alert(message, null, title, 'OK');
-      } else {
-        alert(title ? (title + ": " + message) : message);
-      }
-    },
+registerEvents: function() {
+    var self = this;
+    // Check of browser supports touch events...
+    if (document.documentElement.hasOwnProperty('ontouchstart')) {
+        // ... if yes: register touch event listener to change the "selected" state of the item
+        $('body').on('touchstart', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
+        });
+        $('body').on('touchend', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+        });
+    } else {
+        // ... if not: register mouse events instead
+        $('body').on('mousedown', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
+        });
+        $('body').on('mouseup', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+        });
+    }
+
+    $(window).on('hashchange', $.proxy(this.route, this));
+
+
+},
+
+showAlert: function (message, title) {
+  if (navigator.notification) {
+    navigator.notification.alert(message, null, title, 'OK');
+} else {
+    alert(title ? (title + ": " + message) : message);
+}
+},
     /*    initialize: function() {
         this.store = new MemoryStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
@@ -57,16 +73,42 @@ var app = {
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }*/
-    initialize: function() {
-    
-    this.homeTpl = Handlebars.compile($("#home-tpl").html());
-    this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
 
-    var self = this;
-    this.store = new MemoryStore(function() {
-        self.renderHomeView();
-    });
-}
+    route: function() {
+        var hash = window.location.hash;
+        if (!hash) {
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+        var match = hash.match(app.detailsURL);
+        if (match) {
+            this.store.findById(Number(match[1]), function(employee) {
+                $('body').html(new EmployeeView(employee).render().el);
+            });
+        }
+    },
+
+    initialize: function() {
+
+        var self = this;
+
+        this.detailsURL = /^#employees\/(\d{1,})/;
+        self.registerEvents();
+
+        this.store = new MemoryStore(function() {
+           self.route();
+        });
+
+/*document.addEventListener("deviceready", onDeviceReady, false);*/
+
+
+
+    }
 };
+
+
+/*function onDeviceReady() {
+    console.log(navigator.contacts);
+}*/
 
 app.initialize();
