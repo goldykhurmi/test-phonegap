5e77c73166134262e625f16771d9975d60dc8ced
Sukhbir Singh <goldykhurmi@gmail.com>
2016-03-05 13:26:49 +0530

    t

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,6 @@
 var app = {
 
-    findByName: function() {
+    /*findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
             var l = employees.length;
@@ -11,13 +11,62 @@ var app = {
                 $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
             }
         });
+    },*/
+
+    findByName: function() {
+    var self = this;
+    this.store.findByName($('.search-key').val(), function(employees) {
+        $('.employee-list').html(self.employeeLiTpl(employees));
+    });
     },
 
-    initialize: function() {
+
+
+    /*renderHomeView: function() {
+    var html =
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+            "</div>"
+    $('body').html(html);
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },*/
+
+    renderHomeView: function() {
+    $('body').html(this.homeTpl());
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+
+    showAlert: function (message, title) {
+      if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+      } else {
+        alert(title ? (title + ": " + message) : message);
+      }
+    },
+    /*    initialize: function() {
         this.store = new MemoryStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+    } */
+    /*initialize: function() {
+      var self = this;
+      this.store = new MemoryStore(function() {
+          self.showAlert('Store Initialized', 'Info');
+        });
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    }*/
+    initialize: function() {
+    
+    this.homeTpl = Handlebars.compile($("#home-tpl").html());
+    this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
 
+    var self = this;
+    this.store = new MemoryStore(function() {
+        self.renderHomeView();
+    });
+}
 };
 
 app.initialize();
